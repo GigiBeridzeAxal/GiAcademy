731c93167ff2bd94a0115936f4a03971906d569b
GigiBeridzeAxal <145029127+GigiBeridzeAxal@users.noreply.github.com>
2024-01-14 18:16:33 +0400

    First Commit

--- app/page.js
+++ app/page.js
@@ -1,5 +1,7 @@
 'use client'
 
+
+
 import Image from 'next/image'
 import Header from './components/Header'
 import Welcome from './components/Welcome'
@@ -10,6 +12,10 @@ import About from './components/About'
 export default function Home() {
   return (
     <>
+<link rel="preconnect" href="https://fonts.googleapis.com"/>
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+<link href="https://fonts.googleapis.com/css2?family=Noto+Sans+Georgian:wght@700&display=swap" rel="stylesheet"/>
+
        <Header></Header>
        <Welcome></Welcome>
        <Courses></Courses>
