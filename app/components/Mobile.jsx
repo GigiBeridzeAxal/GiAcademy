8bf2e2bd38397f5c4dede15d13b7cc41aad6fb85
GigiBeridzeAxal <145029127+GigiBeridzeAxal@users.noreply.github.com>
2024-01-16 22:35:25 +0400

    First Commit

--- app/components/Mobile.jsx
+++ app/components/Mobile.jsx
@@ -5,6 +5,7 @@ export default function Mobile() {
     <>
     <div className='Mobile' >
         
+       
         <div className="header">
         <nav class="flex items-center justify-between flex-wrap bg-plate-700 p-6">
   <div class="flex items-center flex-shrink-0 text-white mr-6">
@@ -33,6 +34,7 @@ export default function Mobile() {
     </div>
   </div>
 </nav>
+<div className='px-10 py-10 text-xl' >Courses</div>
 <div className="Reactor">
         <div class="bg-gray-800 py-24 sm:py-32">
   <div class="mx-auto max-w-7xl px-6 lg:px-8">
@@ -44,7 +46,7 @@ export default function Mobile() {
       <article class="flex max-w-xl flex-col items-start justify-between">
         <div class="flex items-center gap-x-4 text-xs">
           <time datetime="2020-03-16" class="text-gray-500">July 16 , 2024</time>
-          <a href="#" class="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100">Programing</a>
+          <a href="#" class="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100">Front-End Development</a>
         </div>
         <div class="group relative">
           <h3 class="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
@@ -53,7 +55,7 @@ export default function Mobile() {
               Make Big Projects With Your Mentors
             </a>
           </h3>
-          <p class="mt-5 line-clamp-3 text-sm leading-6 text-gray-400">Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.</p>
+          <p class="mt-5 line-clamp-3 text-sm leading-6 text-gray-400">Learn Most In Demand Language For Front-End Development When You Will Finish Course You Can Build Front-End Applications And Apis</p>
         </div>
         <div class="relative mt-8 flex items-center gap-x-4">
           <div alt="" class=" gigi h-10 w-10 rounded-full ">
@@ -63,6 +65,50 @@ export default function Mobile() {
               <a className='text-slate-500'  href="#">
                 <span class="absolute inset-0"></span>
                 Gigi Beridze
+              </a>
+            </p>
+            <p class="text-gray-600">Co-Founder / Gi-Academy</p>
+          </div>
+        </div>
+      </article>
+
+
+    </div>
+  </div>
+</div>
+        </div>
+        <br />
+
+        <div className="Reactor">
+        <div class="bg-gray-800 py-24 sm:py-32">
+  <div class="mx-auto max-w-7xl px-6 lg:px-8">
+    <div class="mx-auto max-w-2xl lg:mx-0">
+      <h2 class=" flex reactname  text-3xl font-bold tracking-tight text-gray-300 sm:text-4xl">Laravel <div className=' lar' ></div> </h2> 
+      <p class="mt-2 text-lg leading-8 text-gray-600">Learn Best Php Framework</p>
+    </div>
+    <div class="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+      <article class="flex max-w-xl flex-col items-start justify-between">
+        <div class="flex items-center gap-x-4 text-xs">
+          <time datetime="2020-03-16" class="text-gray-500">July 16 , 2024</time>
+          <a href="#" class="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100">Back-End Development</a>
+        </div>
+        <div class="group relative">
+          <h3 class="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
+            <a className='text-slate-200'  href="#">
+              <span class="absolute text inset-0"></span>
+              Make Big Back-End Appliactions With Database
+            </a>
+          </h3>
+          <p class="mt-5 line-clamp-3 text-sm leading-6 text-gray-400">Laravel/Php Are Strongest Back-End Development Technologys You Can Make Ecomercee Websites Authetication And more After You Study Laravel You Can Make Projcets So Easy</p>
+        </div>
+        <div class="relative mt-8 flex items-center gap-x-4">
+          <div alt="" class=" goga h-10 w-10 rounded-full ">
+          </div>
+          <div class="text-sm leading-6">
+            <p class="font-semibold text-gray-900">
+              <a className='text-slate-500'  href="#">
+                <span class="absolute inset-0"></span>
+                Goga Chxaidze
               </a>
             </p>
             <p class="text-gray-600">Co-Founder / Gi-Academy</p>
